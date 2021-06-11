c07ac7469d14e841c220de7d4743f6b9ccb583d7
Kawalpreet987 <85734139+Kawalpreet987@users.noreply.github.com>
2021-06-11 14:11:47 +0530

    Add files via upload

---
+++ bhagwanproducts.php
@@ -0,0 +1,155 @@
+<html>
+	<head>
+		<title></title>
+		<style>
+		.container{
+			height:850px;
+			width:860px;
+			margin:auto;
+		}
+		.header
+		{
+			height:100px;
+			width:860px;
+			}
+		.sub_header_left
+		{
+			height:100px;
+			width:100px;
+			float:left;
+		}
+		.sub_header_left img
+		{
+			width:100px;
+		}
+		.text
+		{
+			color:gray;
+			padding-left:10px;
+			padding-top:3px;
+			line-height:30px;
+			font-weight:bold;
+		}
+		.sub_header_center
+		{
+			height:100px;
+			width:640px;
+			float:left;
+			color:gray;
+			padding-left:20px;
+		}
+		.sub_header_right
+		{
+			height:100px;
+			width:100px;
+			float:left;
+		}
+		.right_1
+		{
+			float:left;
+			line-height:5px;
+		}
+		.right_1 img, .right_2 img
+		{
+			height:30px;
+			width:30px;
+			padding-left:13px;
+			padding-top:10px;
+		}
+		.right_2
+		{
+			float:left;
+			line-height:5px;
+			
+		}
+		.nav{
+			height:50px;
+			width:860px;
+			background:blue;
+		}
+		.subnav
+		{
+			float:left;
+			border-right:1px solid white;
+			width:121.859px;
+			height:50px;
+			text-align:center;
+			line-height:50px;
+			color:white;
+			text-weight:bold;
+			background:#17E262;
+		}
+		.slider{
+			height:300px;
+			width:860px;
+			background:orange;
+		}
+		.content
+		{
+			height:300px;
+			width:860px;
+			background:gray;
+		}
+		.footer{
+			height:100px;
+			width:860px;
+			background:black;
+		}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class="header">
+				<div class="sub_header_left">
+					<img src="images/tag1.PNG">
+					<br>
+					<div class="text">
+					GRINDERS
+					</div>
+				</div>
+				<div class="sub_header_center">
+					<h1>BHAGWAN PRODUCTS (INDIA)</h1>
+				</div>
+				<div class="sub_header_right">
+					<div class="right_1">
+						<img src="images/home.png">
+						<p>Home</p>
+					</div>
+					<div class="right_2">
+						<img src="images/mail.png">
+						<p>Email</p>
+					</div>
+				</div>
+			</div>
+			<div class="nav">
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+				<div class="subnav">
+				Home
+				</div>
+			</div>
+			<div class="slider">
+			</div>
+			<div class="content">
+			</div>
+			<div class="footer">
+			</div>
+		</div>
+	</body>
+</html>
